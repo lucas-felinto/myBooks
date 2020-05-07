4de7d1fa3e2516666a5b62f82cdfb0514410ef0b
lucasfelinto <lucasfepinto@gmai.com>
2020-05-07 15:27:42 -0300

    feat new book

---
+++ app/controllers/books.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const data = require('../../data.json') 
+
+exports.home = function(req, res) {
+    return res.render("home")
+}
+
+exports.new_book = function(req, res) {
+        return res.render("new_book")
+}
+
+exports.post = function(req, res){
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('PLease, fill out all fields.')
+        }
+    }
+
+    data.books.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err){
+        if (err) return res.send("Erro")
+
+        return res.redirect("/home")
+    })
+
+}
--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const routes = express.Router()
+const books = require('./controllers/books')
 
 routes.get("/", function(req, res){
     return res.redirect("/home")
 })
 
-routes.get("/home", function(req, res) {
-    return res.render("home")
-})
+routes.get("/home", books.home)
+
+routes.get("/new_book", books.new_book)
+
+routes.post("/new_book", books.post)
 
 module.exports = routes
